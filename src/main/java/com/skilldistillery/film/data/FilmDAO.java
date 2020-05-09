cb830443416ccf79a14b96c71fde34d72811dd66
Vincent Borden <vinbor81@gmail.com>
2020-05-09 15:26:36 -0400

    built out more methods in FilmDAOImpl

--- src/main/java/com/skilldistillery/film/data/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAO.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.film.data;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.skilldistillery.film.entities.Actor;
@@ -7,10 +8,10 @@ import com.skilldistillery.film.entities.Film;
 
 public interface FilmDAO {
 	public Film findFilmByID(int filmID);
-	public List<Film> findFilmsByKeyword(String keyword);
+	public List<Film> findFilmsByKeyword(String keyword) throws SQLException;
 	public Film findCreatedFilmsByID(int filmID);
-	public List<Film> findCreatedFilmsByKeyword(String keyword);
-	public Actor findActorByID(int actorID);
+	public List<Film> findCreatedFilmsByKeyword(String keyword) throws SQLException;
+	public Actor findActorByID(int actorID) throws SQLException;
 	public List<Actor> findActorsByFilmID(int filmID);
 	
 	public int createFilm(Film film);
