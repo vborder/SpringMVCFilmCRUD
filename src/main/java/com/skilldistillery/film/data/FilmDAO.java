edcef72a141a78e5148b3da73e490c1654df668e
mikematosky <mikematosky@hotmail.com>
2020-05-08 20:58:10 -0400

    dao interface update

--- src/main/java/com/skilldistillery/film/data/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAO.java
@@ -1,5 +1,18 @@
 package com.skilldistillery.film.data;
 
-public interface FilmDAO {
+import java.util.List;
 
+import com.skilldistillery.film.entities.Film;
+
+public interface FilmDAO {
+	public Film findFilmByID(int filmID);
+	public List<Film> findFilmsByKeyword(String keyword);
+	public Film findCreatedFilmsByID(int filmID);
+	public List<Film> findCreatedFilmsByKeyword(String keyword);
+	public Actor findActorByID(int actorID);
+	public List<Actor> findActorsByFilmID(int filmID);
+	
+	public int addFilm(Film film);
+	public void updateFilm(Film film);
+	public String deleteFilm(int FilmID);
 }
