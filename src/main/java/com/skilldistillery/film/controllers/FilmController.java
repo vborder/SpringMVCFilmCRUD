fe5e2757aeee724b63c3613b778f53c72720c333
mikematosky <mikematosky@hotmail.com>
2020-05-09 13:42:09 -0400

    findbyid

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.skilldistillery.film.data.FilmDAOImpl;
+import com.skilldistillery.film.data.FilmDAO;
 
 @Controller
 public class FilmController {
 
+	@Autowired
+	FilmDAO dao;
 	
 	@RequestMapping(path ="home.do", method= RequestMethod.GET)
 	public ModelAndView goHome() {
