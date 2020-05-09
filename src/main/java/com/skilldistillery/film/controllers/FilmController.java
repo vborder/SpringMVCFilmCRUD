d7fd601dcdb655e25927edf39bf2ddcaf3cf5742
mikematosky <mikematosky@hotmail.com>
2020-05-09 13:02:57 -0400

    darn save button

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,5 +1,23 @@
 package com.skilldistillery.film.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.skilldistillery.film.data.FilmDAOImpl;
+
+@Controller
 public class FilmController {
 
+	@Autowired
+	FilmDAOImpl dao;
+	
+	@RequestMapping(path ="home.do", method= RequestMethod.GET)
+	public ModelAndView goHome() {
+		ModelAndView mav= new ModelAndView();
+		mav.setViewName("WEB-INF/home.jsp");
+		return mav;
+	}
 }
