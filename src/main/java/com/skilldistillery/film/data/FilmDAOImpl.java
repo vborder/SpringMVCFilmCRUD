bb096fc0640760264135993b808882532bf9f4f4
Jacob Zieleniewski <jacobzieleniewski@gmail.com>
2020-05-10 15:47:52 -0600

    deleting film and editing film working

--- src/main/java/com/skilldistillery/film/data/FilmDAOImpl.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAOImpl.java
@@ -274,8 +274,7 @@ public class FilmDAOImpl implements FilmDAO {
 		st.setDouble(8, film.getReplacementCost());
 		st.setString(9, "G");
 		st.setString(10, film.getSpecialFeatures());
-
-//		System.out.println(st);
+		System.out.println(st);
 		int updateCount = st.executeUpdate();
 
 		if (updateCount == 1) {
