cdc8b5baad8a339e21d7ba746736ccd504a95645
Vincent Borden <vinbor81@gmail.com>
2020-05-10 18:13:44 -0400

    changed sql for getFilmByID

--- src/main/java/com/skilldistillery/film/data/FilmDAOImpl.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAOImpl.java
@@ -61,8 +61,8 @@ public class FilmDAOImpl implements FilmDAO {
 				film.setTitle(rs.getString("title"));
 				film.setDescription(rs.getString("description"));
 				film.setReleaseYear(rs.getInt("release_year"));
-				film.setLanguageId(rs.getInt("id"));
-				film.setRentalDuration(rs.getInt("language_id"));
+				film.setLanguageId(rs.getInt("language_id"));
+				film.setRentalDuration(rs.getInt("rental_duration"));
 				film.setRentalRate(rs.getDouble("rental_rate"));
 				film.setLength(rs.getInt("length"));
 				film.setReplacementCost(rs.getDouble("replacement_cost"));
