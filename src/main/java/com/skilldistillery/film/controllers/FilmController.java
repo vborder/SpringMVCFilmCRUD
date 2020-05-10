472266ae09ffa70ce0754e5ef856bd51ff9408df
mikematosky <mikematosky@hotmail.com>
2020-05-10 15:00:22 -0400

    Delete?

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -20,106 +21,100 @@ public class FilmController {
 
 	@Autowired
 	FilmDAO dao;
-	
-	//Homepage
-	@RequestMapping(path ="home.do", method= RequestMethod.GET)
+
+	// Homepage
+	@RequestMapping(path = "home.do", method = RequestMethod.GET)
 	public ModelAndView goHome() {
-		ModelAndView mav= new ModelAndView();
+		ModelAndView mav = new ModelAndView();
 		mav.setViewName("WEB-INF/home.jsp");
 		return mav;
 	}
-	
-	//Results for Search by ID
-	@RequestMapping(path="findByID.do", params="input", method= RequestMethod.GET)
+
+	// Results for Search by ID
+	@RequestMapping(path = "findByID.do", params = "input", method = RequestMethod.GET)
 	public ModelAndView getFilmByID(String input) {
-		ModelAndView mav= new ModelAndView();
-		try { int filmID = Integer.parseInt(input); 
-		Film film=null;
-		
-		if(filmID <= 1000) {
-			film= dao.findFilmByID(filmID);
-		}
-		else {
-			film= dao.findCreatedFilmsByID(filmID);
-		}
-		if (film!=null) {
-		mav.addObject("film", film);
-		mav.setViewName("WEB-INF/result.jsp");
-		}
-		else mav.setViewName("WEB-INF/resultNotFound.jsp");
-		}
-		catch (Exception e) {
+		ModelAndView mav = new ModelAndView();
+		try {
+			int filmID = Integer.parseInt(input);
+			Film film = null;
+
+			if (filmID <= 1000) {
+				film = dao.findFilmByID(filmID);
+			} else {
+				film = dao.findCreatedFilmsByID(filmID);
+			}
+			if (film != null) {
+				mav.addObject("film", film);
+				mav.setViewName("WEB-INF/result.jsp");
+			} else
+				mav.setViewName("WEB-INF/resultNotFound.jsp");
+		} catch (Exception e) {
 			mav.setViewName("WEB-INF/resultNotFound.jsp");
 		}
 		return mav;
 	}
-	
-	//Results for Search by Keyword
-	@RequestMapping(path="findByKeyword.do", params="keyword", method= RequestMethod.GET)
+
+	// Results for Search by Keyword
+	@RequestMapping(path = "findByKeyword.do", params = "keyword", method = RequestMethod.GET)
 	public ModelAndView getFilmsByKeyword(String keyword) throws SQLException {
-		ModelAndView mav= new ModelAndView();
-		List<Film> films= new ArrayList<>();
-		
+		ModelAndView mav = new ModelAndView();
+		List<Film> films = new ArrayList<>();
+
 		films.addAll(dao.findFilmsByKeyword(keyword));
 		films.addAll(dao.findCreatedFilmsByKeyword(keyword));
 		if (films.size() > 0) {
-		mav.addObject("filmList", films);
-		mav.setViewName("WEB-INF/keywordResult.jsp");
-		}
-		else {
+			mav.addObject("filmList", films);
+			mav.setViewName("WEB-INF/keywordResult.jsp");
+		} else {
 			mav.setViewName("WEB-INF/resultNotFound.jsp");
-			
+
 		}
 		return mav;
-		
+
 	}
-	
-	//Redirect and FlashAttributes for Adding a Film
-	@RequestMapping(path="addFilm.do", method= RequestMethod.POST )
-	public ModelAndView createFilm(Film film, RedirectAttributes redir) throws SQLException{
+
+	// Redirect and FlashAttributes for Adding a Film
+	@RequestMapping(path = "addFilm.do", method = RequestMethod.POST)
+	public ModelAndView createFilm(Film film, RedirectAttributes redir) throws SQLException {
 		ModelAndView mav = new ModelAndView();
 		redir.addFlashAttribute("newFilm", film);
 		film = dao.createFilm(film);
-		mav.setViewName ("redirect:filmAdded.do");
-		
+		mav.setViewName("redirect:filmAdded.do");
+
 		return mav;
 	}
-	
-	
-	//Film added
-	@RequestMapping(path="filmAdded.do", method= RequestMethod.GET)
+
+	// Film added
+	@RequestMapping(path = "filmAdded.do", method = RequestMethod.GET)
 	public ModelAndView filmAdded(@ModelAttribute("newFilm") Film film) {
-		ModelAndView mav= new ModelAndView();
+		ModelAndView mav = new ModelAndView();
 		mav.addObject("film", film);
 		mav.setViewName("WEB-INF/result.jsp");
-		
+
 		return mav;
 	}
-	
-	//^^^^^^^^^^^^^^^^^^^^Note for Mike- don't touch anything above^^^^^^^^^^^^^^^^
-	
-	@RequestMapping(path="editFilm.do", method= RequestMethod.GET)
-	public ModelAndView editFilm(Film film) throws SQLException {
-		ModelAndView mav= new ModelAndView();
-		
-		int search= film.getId();
-		
-		if(search <= 1000) {
-			film= dao.findFilmByID(search);
-			dao.updateFilm(film);
-		}
-		else {
-			film= dao.findCreatedFilmsByID(search);
-			dao.updateFilm(film);
-		}
-		mav.addObject("film", film);
-		
-		mav.setViewName("WEB-INF/result.jsp");
-		
-		
-		return mav;
-	}
-	
+
+	// ^^^^^^^^^^^^^^^^^^^^Note for Mike- don't touch anything above^^^^^^^^^^^^^^^^
+//	
+//	@RequestMapping(path="editFilm.do", method= RequestMethod.GET)
+//	public ModelAndView editFilm(Film film) {
+//		ModelAndView mav= new ModelAndView();
+//		
+//		int search= film.getId();
+//		
+//		if(search <= 1000) {
+//			film= dao.findFilmByID(search);
+//		}
+//		else {
+//			film= dao.findCreatedFilmsByID(search);
+//		}
+//		mav.addObject("film", film);
+//		mav.setViewName("WEB-INF/result.jsp");
+//		
+//		
+//		return mav;
+//	}
+//	
 //	@RequestMapping(path="editedFilm.do", method= RequestMethod.POST)
 //	public ModelAndView editedFilm(Film film) throws SQLException{
 //		ModelAndView mav= new ModelAndView();
@@ -134,19 +129,22 @@ public class FilmController {
 //		return mav;
 //	}
 	
-	
-//	@RequestMapping(path= "deleteFilm.do", method= RequestMethod.POST)
-//	public String deleteFilm(Film film, RedirectAttributes redir) {
-//		redir.addFlashAttribute("???", dao.deleteFilm(film);
-//		return "redirect:deleted.do";
-//	}
-//	
+	@RequestMapping(path="updateFilm.do", method= RequestMethod.POST)
+	public ModelAndView updateFilm(Film film)
 
-//	@RequestMapping(path= "deleted.do", method= RequestMethod.DELETE)
-//	public ModelAndView deleteFilm {
+	@RequestMapping(path = "deleteFilm.do", method = RequestMethod.POST)
+	public ModelAndView deleteFilm(@RequestParam("id") int filmId) throws SQLException {
+		ModelAndView mav = new ModelAndView();
+		Film film = dao.findCreatedFilmsByID(filmId);
+		boolean deleted = dao.deleteFilm(film);
+		if (deleted) {
+			mav.addObject("deleted", deleted);
+			mav.setViewName("result.jsp");
+		}
+		else {
+			mav.setViewName("deleteFailed.jsp");
+		}
+		return mav;
+	}
 
-
-//	
-//	}
-	
 }
